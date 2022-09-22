dca5a71c117f7a696b3b22c5e39376a65e823d1a
Mudit Choudhary <cmudit60@gmail.com>
2022-09-22 07:56:09 +0530

    Made the contact section

--- src/index.js
+++ src/index.js
@@ -5,10 +5,21 @@ import { ImageController } from './classes/imageController';
 import { AnimationController } from './classes/animationController';
 import {NavigationController} from './classes/navigationController'
 import {Projects} from './classes/projects';
+import githubIcon from './Assets/Icons/github.svg'
+import twitterIcon from './Assets/Icons/twitter.svg'
+import linkedinIcon from './Assets/Icons/linkedin.svg'
+import instagramIcon from './Assets/Icons/instagram.svg'
 
 ImageController.addImg(document.querySelector('.my-profile > img'), myProfileImg);
 AnimationController.keyHightLightAnim("Tech Enthusiast");
 NavigationController.navBarToggler();
 Projects.madeProjedtSection();
+
+// Adding social icons
+const socialIcons = [githubIcon, twitterIcon, linkedinIcon, instagramIcon];
+for (let i = 0; i < 4; i++) {
+    ImageController.addImg(document.querySelector(`#social${i + 1}`), socialIcons[i]);
+}
+
 
 
